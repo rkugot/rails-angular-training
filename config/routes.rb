69da12da7ab89c537ce89400fca92dabfd98a9c8
Roman Kugot <roman_kugot@epam.com>
2019-05-27 18:37:57 +0300

    Clean up routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root 'home#index'
-  get 'home/show'
+  post '/login', to: 'users#login'
+  get '*unmatched_route', to: 'home#index'
 end
